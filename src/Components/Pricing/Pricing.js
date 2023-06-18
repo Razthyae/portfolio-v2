161d8b2afe0e4580ebbf352f25ee99f1d5a778b6
Akash Kumar <208brassband@gmail.com>
2023-06-19 00:16:49 +0200

    Added pricing section

--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -1,49 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import './Pricing.css'
+import { Questions } from "./Questions";
 import { motion } from "framer-motion";
 
-export const Pricing = () => {
+export function Pricing() {
+    let [question, setQuestion] = useState(1)
+    let [username, setUsername] = useState('')
+    let [email, setEmail] = useState('')
+    let [phone, setPhone] = useState('')
+
+    const handleSubmit = () => {
+        if (username === '' || email === '' || phone === '') {
+            alert("Brak wymaganych danych!")
+        } else {
+            alert(`Dziękuję za wypełnienie formularza!`)
+        }
+    }
+
     return (
-<motion.section id="pricing"
-            key="pricing"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 1 }}
-            exit={{ opacity: 0, transition: { duration: 0.5 } }}
+        <motion.section id="pricing"
+        key="pricing"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 1 }}
+        exit={{ opacity: 0, transition: {duration: 0.5} }}
         >
+            <h1 id='pricingHeader'>For pricing please answer few questions about your project. <br></br>The more info you provide me, the easier it will be to work together! :)</h1>
 
-        <section id="pricing">
-           <div>
-            Pricing
-           </div>
-           <div className='contactWrapper'>
-                <div className="phone cta">Call me</div>
-                <div className="phone number">+48 796 453 528</div>
-            </div>
-
-                <a href='https://m.me/ZealDJ'>
-                <div className='contactWrapper'>
-                    <div className="phone cta">Messenger</div>
-                    <div className="phone number"> Click me!</div>
-                </div>
-            </a>
-
-            <a href='mailto:a_kumar@poczta.fm'>
-                <div className='contactWrapper'>
-                    <div className="phone cta">Mail</div>
-                    <div className="phone number">a_kumar@poczta.fm</div>
-                </div>
-            </a>
-
-            <a href='https://razthyae.github.io/'>
-                <div className='contactWrapper'>
-                    <div className="phone cta">GitHub</div>
-                    <div className="phone number">https://razthyae.github.io/</div>
-                </div>
-            </a>
-        </section>
-
-
+            <Questions
+                question={question}
+                setQuestion={setQuestion}
+                username={username}
+                setUsername={setUsername}
+                email={email}
+                setEmail={setEmail}
+                phone={phone}
+                setPhone={setPhone}
+                handleSubmit={handleSubmit}
+            />
+           
         </motion.section>
     )
 }
