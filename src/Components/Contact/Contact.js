cba122ba4d5302031bde9313a9558c467f20b043
Akash Kumar <208brassband@gmail.com>
2023-07-13 13:14:09 +0200

    Finished contact translations

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,11 @@
 import React from "react";
 import './Contact.css'
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 export const Contact = () => {
+const {t} = useTranslation()
+
     return (
         <motion.section id="contact"
             key="contact"
@@ -15,20 +18,20 @@ export const Contact = () => {
 
 
             <div className='contactWrapper'>
-                <div className="phone cta ">Call me</div>
+                <div className="phone cta ">{t('contact-call')}</div>
                 <div className="phone number">+48 796 453 528</div>
             </div>
 
             <a href='https://m.me/ZealDJ'>
                 <div className='contactWrapper'>
                     <div className="phone cta ">Messenger</div>
-                    <div className="phone number"> Click me!</div>
+                    <div className="phone number"> {t('contact-messenger')}</div>
                 </div>
             </a>
 
             <a href='mailto:a_kumar@poczta.fm'>
                 <div className='contactWrapper'>
-                    <div className="phone cta ">Mail</div>
+                    <div className="phone cta ">{t('contact-email')}</div>
                     <div className="phone number">a_kumar@poczta.fm</div>
                 </div>
             </a>
