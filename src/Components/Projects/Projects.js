19e0a2957d57f9027fb42de0cd575b8b99f43342
Akash Kumar <a_kumar@poczta.fm>
2023-10-06 00:34:41 +0200

    Changed project section design

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -19,28 +19,29 @@ const {t} = useTranslation()
       description:
         t('projects-1-description'),
       button: t('projects-visit'),
+      image: "https://i.imgur.com/9VjHrAd.png",
+      user: "Myself :)",
+      userProfile: "https://i.imgur.com/sU8LWZt.jpg",
+      website: "https://couplesgame.netlify.app/"
+    },
+    {
+      title: t('projects-2-title'),
+      description: t('projects-2-description'),
+      button: t('projects-visit'),
       image: "https://i.imgur.com/2lFHrIs.jpg",
       user: "Just Move",
+      userProfile: "https://i.imgur.com/0Clfnu7.png",
       userProfile: "https://i.imgur.com/sU8LWZt.jpg",
       website: "https://jmgrafik.netlify.app/"
     },
     {
-      title: "Tortor Dapibus Commodo Aenean Quam",
-      description:
-        "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
-      button: "Visit site",
-      image: "https://i.imgur.com/DCdBXcq.jpg",
-      user: "Erich Behrens",
-      userProfile: "https://i.imgur.com/0Clfnu7.png"
-    },
-    {
-      title: "Phasellus volutpat metus",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
-      button: "Visit site",
-      image: "https://i.imgur.com/eaFYHmN.jpg",
+      title: t('projects-3-title'),
+      description: t('projects-3-description'),
+      button: t('projects-visit'),
+      image: "https://i.imgur.com/rbvC7fw.png",
       user: "Bruno Vizovskyy",
-      userProfile: "https://i.imgur.com/4KeKvtH.png"
+      userProfile: "https://i.imgur.com/4KeKvtH.png",
+      website: "https://task-better-weigh.netlify.app/"
     }
   ];
 
@@ -52,7 +53,7 @@ const {t} = useTranslation()
         exit={{opacity: 0, transition: {duration: 0.5}}}
         >
           <div id='projectsContainer'>
-           <h1 id='projectsHeader'>{t('projects-header')}</h1>
+         
             <Slider className="slider-wrapper">
       {content.map((item, index) => (
         <div
